96ee5f576a385488efcf7e828694854f475e1cfc
rbirchtree <rbirchtree@gmail.com>
2019-01-11 15:51:05 -0800

    inital commit with readme and demo files

---
+++ README.md
@@ -0,0 +1,6 @@
+# ReadMe
+Valuation Chart
+
+1. git clone..
+2. npm i
+3. Make the correct config and .env files.
---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname,'client/build')));
+
+app.get('/api/oil-field-valuation',(req,res) =>{
+	res.send(200);
+});
+
+app.get('*',(req,res) => {
+	res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`API is listening on ${port}`);
+
+
